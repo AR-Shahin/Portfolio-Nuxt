dcb1810dc6d8974861b894ee8b0f04717ce4e1f0
AR-Shahin <mdshahinmije96@gmail.com>
2025-01-25 20:30:03 +0600

    up

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nuxt-link to="/">Home</nuxt-link>
+       <h1> About page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nuxt-link to="/about">About</nuxt-link>
+       <h1> Home page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
