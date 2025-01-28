303b6345fd8c66200d41b331b41ca610a6bbd07b
AR-Shahin <mdshahinmije96@gmail.com>
2025-01-28 19:33:29 +0600

    up

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nuxt-link to="/">Home</nuxt-link>
+        Test
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
