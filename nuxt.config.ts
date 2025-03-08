bcc724304da1359e8b3c6d683c398a93ce29fdca
AR-Shahin <mdshahinmije96@gmail.com>
2025-03-08 13:57:59 +0600

    Quick issue fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,56 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  dev: false,
-  debug: true, 
+  nitro: {
+    preset: 'netlify'
+  },
+  app:{
+    pageTransition: { name: 'fade', mode: 'out-in' },
+    head:{
+      title : "AR Shahin",
+      link: [
+       
+        {
+          href : "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500&display=swap",
+          rel: "stylesheet"
+        },
+        {
+          href : "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css",
+          rel: "stylesheet"
+        },
+        {
+          href : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+          rel: "stylesheet"
+        },
+        {
+          href : "https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css",
+          rel: "stylesheet"
+        },
+
+        { rel: 'icon', type: 'image/png', href: "https://www.freeiconspng.com/uploads/fast-food-png-most-popular-fast-food-snacks-in-your-area-and-most--3.png" }
+      ],
+      script: [
+        {
+          src : "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        },
+        {
+          src : "/js/_main.js"
+        },
+    
+      ]
+
+    }
+  },
+  css:[
+    "@/assets/css/main.css",
+    "@/assets/css/responsive.css",
+
+  ],
+
+  plugins: [
+    
+  ],
+  modules: [ '@pinia/nuxt', ],
+
+ 
 })
-
-
