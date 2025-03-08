d9d90f8f84a1a516f5325af336050eedccf87759
AR-Shahin <mdshahinmije96@gmail.com>
2025-03-08 14:10:53 +0600

    Quick issue fix

---
+++ components/MainNavbar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <router-link class="navbar-brand" to="/">
+                <img src="~assets/img/logo.png" alt="">
+            </router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav" style="margin-left:auto">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="https://tutspack.com/" target="_blank">Blog</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#service_section">Services</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#youtube_section">Courses</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#portfolio_section" tabindex="-1" aria-disabled="true">Portfolio</a>
+                </li>
+                 <li class="nav-item">
+                <a class="nav-link" href="#contact_section" tabindex="-1" aria-disabled="true">Contact</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        
+        <MainNavbar />
+        <main id="main" class="main">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
